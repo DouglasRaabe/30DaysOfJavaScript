f228156bc93623dcd67fadc7944603098538757a
Douglas Raabe <douglas.pods@gmail.com>
2021-07-06 09:59:36 -0300

    Aula do dia 01

---
+++ Aula01/datatypes.js
@@ -0,0 +1,6 @@
+//Dia01 05
+console.log(typeof 'Asabeneh') // string
+console.log(typeof 5) // number
+console.log(typeof true) // boolean
+console.log(typeof null) // object type
+console.log(typeof undefined) // undefined
